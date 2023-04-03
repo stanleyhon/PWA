84a770fadb1bfa10a44bba73565a53c2de3f7190
liahiscock <liahiscock@microsoft.com>
2023-04-03 15:32:22 -0700

    updating app.js file path

--- MediaControls/app.js
+++ MediaControls/app.js
@@ -1,6 +1,6 @@
 const registerServiceWorker = async () => {
     try {
-      await navigator.serviceWorker.register('/sw.js', { scope: '/'});
+      await navigator.serviceWorker.register('/PWA/MediaControls/sw.js', { scope: '/PWA/MediaControls/'});
       console.log('Service worker registered');
     } catch (e) {
       console.log(`Registration failed: ${e}`);
